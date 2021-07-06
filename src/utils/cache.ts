5c5ad834885a41e2868adf9a5f2653250e6db6ca
nick <nick121212@126.com>
2021-07-06 11:23:44 +0800

    Update cache.ts

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -20,53 +20,79 @@ import { removeCacheFromKey } from "./remove";
  * @returns {Promise<any>}
  */
 export const cacheDec = (
-    func: (...args: any[]) => Promise<any>,
     key: string,
+    func: (...args: any[]) => Promise<any>,
     settings: CacheOptionModel,
     storage?: Keyv,
     pCache: Store<any> = promiseCache
 ): ((...args: any[]) => Promise<any>) => {
     const { cache = false, reload = false, expire = 0 } = settings || {};
 
-    async function CacheFunc(...args: any[]) {
-        // 删除缓存
-        if (reload || !cache) {
-            removeCacheFromKey(key, pCache);
-            removeCacheFromKey(key, storage);
-        }
-
-        // 如果不缓存直接调用方法
-        if (!cache) {
-            return func.call(this, ...args);
-        }
-
-        // 先从内存中获取数据，在从local中获取，如果没有再从内存中获取
-        let dataInCache = await getDataFromStorage(key, pCache);
-        if (!dataInCache) {
-            dataInCache = await getDataFromStorage(key, storage);
-        }
-
-        // 命中缓存
-        if (dataInCache) {
-            return Promise.resolve(dataInCache.data);
-        }
-
-        // 如果错误的话，清除掉缓存
+    const funcWithWrap = async (...args: any[]) => {
         const promise = func.call(this, ...args);
 
         // 添加缓存, 为了并发多次请求的情况下，故添加内存的promise缓存
         pCache.set(key, { data: promise, expire, cacheIn: Date.now() });
 
-        promise
+        await promise
             .then((d) => {
-                storage?.set(key, { data: d, expire, cacheIn: Date.now() });
+                return storage?.set(key, {
+                    data: d,
+                    expire,
+                    cacheIn: Date.now(),
+                });
             })
             .catch(async (e) => {
-                await removeCacheFromKey(key, pCache);
+                removeCacheFromKey(key, pCache);
                 await removeCacheFromKey(key, storage);
 
                 throw e;
             });
+
+        return promise;
+    };
+
+    async function CacheFunc(...args: any[]) {
+        // 如果不缓存直接调用方法
+        if (!cache) {
+            return func.call(this, ...args);
+        }
+
+        let cacheInPromise;
+
+        // 需要重新加载的情况
+        if (reload) {
+            cacheInPromise = async () => {
+                removeCacheFromKey(key, pCache);
+
+                await removeCacheFromKey(key, storage);
+
+                return  await funcWithWrap(...args);
+            };
+        } else {
+            cacheInPromise = async () => {
+                let dataInCache = await getDataFromStorage(key, pCache);
+
+                if (!dataInCache && storage) {
+                    dataInCache = await getDataFromStorage(key, storage);
+                }
+
+                // 命中缓存
+                if (dataInCache) {
+                    return Promise.resolve(dataInCache.data);
+                }
+
+                return funcWithWrap(...args);
+            };
+        }
+
+        const promise = cacheInPromise();
+
+        pCache.set(key, {
+            data: promise,
+            expire,
+            cacheIn: Date.now(),
+        });
 
         return promise;
     }
