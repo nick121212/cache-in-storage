b8bbba9ab34b95767fda4c2facf208f52cd8d7c0
nick <nick121212@126.com>
2021-07-06 11:29:09 +0800

    Update func.js

--- tests/func.js
+++ tests/func.js
@@ -1,10 +1,29 @@
+import QuickLRU from "quick-lru";
+const lru = new QuickLRU({ maxSize: 1000 });
+
 export const getTimeSpan = async (isError = false) => {
   return new Promise((resolve, _reject) => {
     setTimeout(() => {
       if (isError) {
         return _reject(new Error("test"));
       }
-      resolve(Date.now());
+      resolve(Date.now() + Math.random());
     }, 200);
   });
 };
+
+
+export const TestStorage = {
+  async get(key) {
+    return lru.get(key);
+  },
+  async set(key, value, _ttl) {
+    lru.set(key, value);
+  },
+  async delete(key) {
+    return lru.delete(key);
+  },
+  async clear() {
+    lru.clear();
+  }
+}
