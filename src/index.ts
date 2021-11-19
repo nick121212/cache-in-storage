a2989b7d22aa4c028039eee77a763b61ed371f9e
nick.feng <nick.feng@coupang.com>
2021-11-19 12:54:14 +0800

    export promiseCache

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { getCacheInWithKey } from "./utils/get-cache-in";
 export { getDataFromStorage } from "./utils/get-data";
 export { removeCacheFromKey } from "./utils/remove";
 export { setExpire } from "./utils/set-expire";
+export { default as promiseCache } from "./cache/promise-cache";
