a647192160b0b91ff73dc38777cec481c987f11a
nick.feng <nick.feng@coupang.com>
2021-11-19 15:54:59 +0800

    export promiseCache

--- tests/index.d.ts
+++
@@ -1,7 +0,0 @@
-import promiseCache from "./cache/promise-cache";
-import { cacheDec } from "./utils/cache";
-import { getCacheInWithKey } from "./utils/get-cache-in";
-import { getDataFromStorage } from "./utils/get-data";
-import { removeCacheFromKey } from "./utils/remove";
-import { setExpire } from "./utils/set-expire";
-export { promiseCache, cacheDec, getCacheInWithKey, getDataFromStorage, removeCacheFromKey, setExpire };
