334c681938bbdb5d1f94d33052dd4807256be341
nick <nick121212@126.com>
2021-11-19 13:14:18 +0800

    Update setcachein.spec.js

--- tests/utils/setcachein.spec.js
+++ tests/utils/setcachein.spec.js
@@ -1,19 +1,23 @@
 import { assert, expect } from "chai";
+import Keyv from "keyv";
 
-import { setExpire, getCacheInWithKey, BaseFactory , getDataFromStorage} from "../../es6/index";
+import { setExpire, getCacheInWithKey , getDataFromStorage} from "../../esm/index";
 
 describe("测试setExpire方法", () => {
-  it("设置expire 从100 到200", () => {
-    const storage = new BaseFactory();
+  it("设置expire 从100 到200", async() => {
+    const storage = new Keyv();
     const cacheIn = Date.now();
 
-    storage.setItem("test2", JSON.stringify({ cacheIn, data: 1, expire: 100 }));
+    await storage.set("test2", JSON.stringify({ cacheIn, data: 1, expire: 100 }));
 
+    let test2 = await storage.get("test2");
 
-    expect(JSON.parse(storage.getItem("test2")).expire).to.be.eq(100);
+    expect(JSON.parse(test2).expire).to.be.eq(100);
 
-    setExpire('test2', 200, storage);
+    await setExpire('test2', 200, storage);
 
-    expect(JSON.parse(storage.getItem("test2")).expire).to.be.eq(200);
+    test2 = await storage.get("test2");
+
+    expect(JSON.parse(test2).expire).to.be.eq(200);
   });
 });
