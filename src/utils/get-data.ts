c83dbb57e98a38ab0b2fead621cf036a56f2d801
nick <nick121212@126.com>
2021-12-06 18:13:34 +0800

    Update get-data.ts

--- src/utils/get-data.ts
+++ src/utils/get-data.ts
@@ -1,18 +1,30 @@
 import Keyv, { Store } from "keyv";
 import { CacheDataModel } from "../models/cache.data";
+import { isPromise } from "./is-promise";
 
 const parseData = (
     key: string,
     dataFromStorage: any,
     cache: Keyv | Store<any>
 ) => {
-    if (typeof dataFromStorage === "string") {
+    if (!dataFromStorage) {
+        return null;
+    }
+
+    if (isPromise(dataFromStorage)) {
+        return dataFromStorage;
+    }
+
+    if (typeof dataFromStorage === "object") {
+    } else if (typeof dataFromStorage === "string") {
         try {
             dataFromStorage = JSON.parse(dataFromStorage);
         } catch (e) {
             cache.delete(key);
             return null;
         }
+    } else {
+        throw new Error("dataFromStorage is not a string filed.");
     }
 
     const { expire = 0, cacheIn = 0, data = null } = dataFromStorage || {};
@@ -36,30 +48,20 @@ const parseData = (
  * @param {String}  key     Key值
  * @returns {null|}
  */
-export const getDataFromStorage = async (
+export const getDataFromStorage = (
     key: string,
     cache?: Keyv | Store<any>
-): Promise<CacheDataModel | null> => {
+): Promise<CacheDataModel | null> | CacheDataModel | null => {
     if (!cache) {
         return null;
     }
+    const data = cache.get(key);
 
-    return parseData(key, await cache.get(key), cache);
-};
-
-/**
- * 从缓存中获取数据
- * @param {Storage} storage 缓存对象
- * @param {String}  key     Key值
- * @returns {null|}
- */
-export const getDataFromStorageSync = (
-    key: string,
-    cache?: Keyv | Store<any>
-): CacheDataModel | null => {
-    if (!cache) {
-        return null;
+    if (isPromise(data)) {
+        return data.then((d: any) => {
+            return parseData(key, d, cache);
+        });
     }
 
-    return parseData(key, cache.get(key), cache);
+    return parseData(key, data, cache);
 };
