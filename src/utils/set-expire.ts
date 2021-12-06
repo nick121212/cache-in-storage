e873d086411fe6a3ec3aed6ee2f0904337601f78
nick <nick121212@126.com>
2021-12-06 18:13:50 +0800

    Update set-expire.ts

--- src/utils/set-expire.ts
+++ src/utils/set-expire.ts
@@ -1,6 +1,7 @@
-import Keyv from "keyv";
+import Keyv, { Store } from "keyv";
 import { CacheDataModel } from "../models/cache.data";
 import { getDataFromStorage } from "./get-data";
+import { isPromise } from "./is-promise";
 
 /**
  * 重新设置缓存中的失效时间
@@ -8,17 +9,38 @@ import { getDataFromStorage } from "./get-data";
  * @param {Number}    expire      缓存过期时间
  * @param {Storage}   storage     缓存的Storage
  */
-export const setExpire = async (key: string, expire: number, cache?: Keyv): Promise<boolean> => {
-    const dataInCache: CacheDataModel | null = cache ? await getDataFromStorage(key, cache) : null;
+export const setExpire = (
+    key: string,
+    expire: number,
+    cache?: Keyv | Store<any>
+): Promise<boolean> | boolean => {
+    const dataInCache = getDataFromStorage(key, cache);
 
     if (!dataInCache || !cache) {
         return false;
     }
 
-    await cache?.set(key, JSON.stringify({ 
-        ...dataInCache,
-        expire
-     }));
+    if (isPromise(dataInCache)) {
+        return (dataInCache as Promise<CacheDataModel | null>).then((d) => {
+            if (!d) {
+                return false;
+            }
 
-    return true;
+            return cache?.set(
+                key,
+                JSON.stringify({
+                    ...d,
+                    expire,
+                })
+            );
+        });
+    }
+
+    return cache?.set(
+        key,
+        JSON.stringify({
+            ...dataInCache,
+            expire,
+        })
+    );
 };
