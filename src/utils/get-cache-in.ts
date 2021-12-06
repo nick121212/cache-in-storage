ce0111c63842339cd1ec3a8d39990dfcc5fdca4d
nick <nick121212@126.com>
2021-12-06 18:13:23 +0800

    Update get-cache-in.ts

--- src/utils/get-cache-in.ts
+++ src/utils/get-cache-in.ts
@@ -1,18 +1,28 @@
 import { Store } from "keyv";
 import { CacheDataModel } from "../models/cache.data";
 import { getDataFromStorage } from "./get-data";
+import { isPromise } from "./is-promise";
 
 /**
  * 从缓存中获取cacheIn字段
  * @param {String}    key         缓存的key
  * @param {Storage}   storage     缓存的Storage
  */
-export const getCacheInWithKey = async (key: string, cache?: Store<any>): Promise<number> => {
-    const dataInCache: CacheDataModel | null = cache ? await getDataFromStorage(key, cache) : null;
+export const getCacheInWithKey = (
+    key: string,
+    cache?: Store<any>
+): Promise<number> | number => {
+    const dataInCache = getDataFromStorage(key, cache);
 
     if (!dataInCache) {
         return 0;
     }
 
-    return dataInCache.cacheIn;
+    if (isPromise(dataInCache)) {
+        return (dataInCache as Promise<CacheDataModel>).then((d) => {
+            return d ? d.cacheIn : 0;
+        });
+    }
+
+    return dataInCache ? (dataInCache as CacheDataModel).cacheIn : 0;
 };
