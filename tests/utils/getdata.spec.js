18498589f2ea2651b58c8fb63e1ec1d452ef393d
nick <nick121212@126.com>
2021-12-06 18:14:51 +0800

    Update getdata.spec.js

--- tests/utils/getdata.spec.js
+++ tests/utils/getdata.spec.js
@@ -4,17 +4,20 @@ import Keyv from "keyv";
 import { getDataFromStorage } from "../../esm/index";
 
 describe("测试getData.js文件", () => {
-  it("getDataFromStorage", async () => {
-    const storage = new Keyv();
-    const cacheIn = Date.now();
+    it("getDataFromStorage", async () => {
+        const storage = new Keyv();
+        const cacheIn = Date.now();
+        const cacheData = { cacheIn, data: 1, expire: 0 };
 
-    const cacheData = { cacheIn, data: 1, expire: 0 };
+        await storage.set("test2.a", cacheData);
+        await storage.set("test2.b.b", cacheData);
+        await storage.set("test2.b.c", cacheData);
 
-    storage.set("test2.a", cacheData);
-    storage.set("test2.b.b", cacheData);
-    storage.set("test2.b.c", cacheData);
+        const dataInCache = await getDataFromStorage("test2.b.c", storage);
 
-    expect(await getDataFromStorage("test2.a.a")).to.be.eq(null);
-    expect((await getDataFromStorage("test2.b.c", storage)).data).to.be.eq(cacheData.data);
-  });
+        expect(getDataFromStorage("test2.a.a")).to.be.eq(null);
+        expect(dataInCache.data).to.be.eq(
+            cacheData.data
+        );
+    });
 });
